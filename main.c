249dde64ff7fe659849ad15466f69bb44709b6b9
Fake ID <junayeed@protonmail.com>
2017-04-03 20:50:49 +0600

    Updated source

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void switchCase(int, int);
+
+int main() {
+
+    int a,b;
+
+    printf("Enter the value of two numbers.");
+    scanf("%d%d", a,b);
+
+    switchCase(a,b);
+    return 0;
+}
+
+void switchCase(int a, int b)
+{
+    int input;
+
+    printf("Enter the operation number you want to perform. 1 for Addition, 2 for subtraction, 3 for multiplication and 4 for division.");
+    scanf("%d", input);
+
+    switch(input) {
+
+        case 1:
+            printf("%d + %d is %d", a, b, a+b);
+            break;
+        case 2:
+            printf("%d - %d is %d", a, b, a-b);
+        case 3:
+            printf("%d * %d is %d", a,b, a*b);
+        case 4:
+            printf("%d / %d is %d", a,b, a/b);
+        default:
+            printf("Wrong choice!");
+            switchCase(a, b);
+    }
+
+
+}
